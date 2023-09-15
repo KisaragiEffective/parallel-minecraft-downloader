086c880bb3a76b9e5aa2b81a009b71e9d9b7415a
Kisaragi Marine <kisaragi.effective@gmail.com>
2023-09-15 20:59:04 +0900

    refactor: split code

--- src/main.rs
+++ src/main.rs
@@ -20,13 +20,14 @@ use base64::Engine;
 use base64::engine::GeneralPurposeConfig;
 use clap::Parser;
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use reqwest::blocking::Client;
 use reqwest::Url;
 use reqwest::tls::Version;
 use serde::Deserialize;
 
 use sha1_smol::Sha1;
 use model::{AssetMappingRoot, DetailedVersionMetadata, PartialVersionManifestRoot};
-use crate::model::{Sha1Hash, VersionIdentifier};
+use crate::model::{AssetMappingValue, Sha1Hash, VersionIdentifier};
 
 
 #[derive(Parser)]
@@ -128,36 +129,38 @@ fn main() {
     }
 
     let force = args.re_download;
-    assets.into_par_iter().for_each(|(_, meta)| {
-        let size = meta.size;
-        let hash = &meta.hash;
-        {
-            let _ = stderr().lock();
-            eprintln!("{hash}: processing");
-        }
+    assets.into_par_iter().for_each(|kv| process(kv, &args, Arc::clone(&client), force));
+}
 
-        let (url, path) = create_channel(hash, &args.dot_minecraft);
+fn process((_, meta): (String, AssetMappingValue), args: &Args, client: Arc<Client>, force: bool) {
+    let size = meta.size;
+    let hash = &meta.hash;
+    {
+        let _ = stderr().lock();
+        eprintln!("{hash}: processing");
+    }
 
-        'download: {
-            if !force {
-                // make sure that
-                if let Ok(res) = client.head(url.clone()).send() {
-                    let headers = res.headers();
-                    if let Some(azure_md5_header) = headers.get("content-md5") {
-                        if let Ok(fd) = File::open(&path) {
-                            let mut br = BufReader::with_capacity(size, fd);
-                            let mut buf = vec![];
+    let (url, path) = create_channel(hash, &args.dot_minecraft);
 
-                            if let Ok(read_size) = br.read_to_end(&mut buf) {
-                                if read_size == size {
-                                    let actual = md5::compute(&buf).0;
-                                    let base64_engine = base64::engine::GeneralPurpose::new(&STANDARD, GeneralPurposeConfig::new());
-                                    if let Ok(decoded_azure_md5_header) = base64_engine.decode(azure_md5_header.as_bytes()) {
-                                        if decoded_azure_md5_header == actual {
-                                            let _ = stderr().lock();
-                                            eprintln!("{hash}: cached; skipping");
-                                            break 'download
-                                        }
+    'download: {
+        if !force {
+            // make sure that
+            if let Ok(res) = client.head(url.clone()).send() {
+                let headers = res.headers();
+                if let Some(azure_md5_header) = headers.get("content-md5") {
+                    if let Ok(fd) = File::open(&path) {
+                        let mut br = BufReader::with_capacity(size, fd);
+                        let mut buf = vec![];
+
+                        if let Ok(read_size) = br.read_to_end(&mut buf) {
+                            if read_size == size {
+                                let actual = md5::compute(&buf).0;
+                                let base64_engine = base64::engine::GeneralPurpose::new(&STANDARD, GeneralPurposeConfig::new());
+                                if let Ok(decoded_azure_md5_header) = base64_engine.decode(azure_md5_header.as_bytes()) {
+                                    if decoded_azure_md5_header == actual {
+                                        let _ = stderr().lock();
+                                        eprintln!("{hash}: cached; skipping");
+                                        break 'download
                                     }
                                 }
                             }
@@ -165,32 +168,37 @@ fn main() {
                     }
                 }
             }
-
-            let bytes = client.get(url).send().expect("failed to get")
-                .bytes().expect("failed to read response");
-
-            let actual_hash = sha1_hash(&bytes);
-            if args.unsafe_danger_skip_validation_hash_and_size || bytes.len() == size && &actual_hash == hash {
-                if args.unsafe_danger_skip_validation_hash_and_size {
-                    let _ = stderr().lock();
-                    eprintln!("{hash}: actual size or hash did not match, but it will be SAVED!! Please be aware that this feature is NOT SUPPORTED. USE AT YOUR OWN PERIL.");
-                }
-                let mut buf_writer = BufWriter::with_capacity(size, File::options().create(true).write(true).open(&path).expect("failed to open"));
-                buf_writer.write_all(&bytes).expect("failed to write to file");
-            } else {
-                let _ = stderr().lock();
-                eprintln!("{hash}: hash mismatch. actual hash is {actual_hash}; skipping");
-            }
         }
-    });
+
+        let bytes = client.get(url).send().expect("failed to get")
+            .bytes().expect("failed to read response");
+
+        let actual_hash = sha1_hash(&bytes);
+        if args.unsafe_danger_skip_validation_hash_and_size || bytes.len() == size && &actual_hash == hash {
+            if args.unsafe_danger_skip_validation_hash_and_size {
+                let _ = stderr().lock();
+                eprintln!("{hash}: actual size or hash did not match, but it will be SAVED!! Please be aware that this feature is NOT SUPPORTED. USE AT YOUR OWN PERIL.");
+            }
+            let mut buf_writer = BufWriter::with_capacity(size, File::options().create(true).write(true).open(&path).expect("failed to open"));
+            buf_writer.write_all(&bytes).expect("failed to write to file");
+        } else {
+            let _ = stderr().lock();
+            eprintln!("{hash}: hash mismatch. actual hash is {actual_hash}; skipping");
+        }
+    }
 }
 
-fn create_channel(hash: &Sha1Hash, base_dir: &Path) -> (Url, PathBuf) {
-    let hash = hash.human_readable();
-    let hash = &hash;
 
+fn extract_starting_two_hex_digit(hash: &Sha1Hash) -> &str {
+    let hash = hash.human_readable();
     let head = unsafe { std::str::from_utf8_unchecked(hash.as_bytes().get_unchecked(0..2)) };
-    let path = base_dir.join("assets").join("objects").join(head).join(hash);
+
+    head
+}
+
+fn create_asset_url(hash: &Sha1Hash) -> Url {
+    let head = extract_starting_two_hex_digit(hash);
+    let hash = hash.human_readable();
 
     const M: &str = "https://resources.download.minecraft.net/";
     let mut raw = String::with_capacity(M.len() + 1 + head.len() + 1 + hash.len());
@@ -198,10 +206,22 @@ fn create_channel(hash: &Sha1Hash, base_dir: &Path) -> (Url, PathBuf) {
     raw += "/";
     raw += unsafe { transmute(head) };
     raw += "/";
-    raw += hash;
+    raw += &hash;
 
     // this never panics.
     let url = Url::parse(&raw).unwrap();
+
+    url
+}
+
+fn create_channel(hash: &Sha1Hash, base_dir: &Path) -> (Url, PathBuf) {
+    // this never panics.
+    let url = create_asset_url(hash);
+    let head = extract_starting_two_hex_digit(hash);
+    let hash = hash.human_readable();
+    let hash = &hash;
+
+    let path = base_dir.join("assets").join("objects").join(head).join(hash);
 
     (url, path)
 }
